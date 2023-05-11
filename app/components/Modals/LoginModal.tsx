9c87d6fb9fdb3693b5ff64a18323746d86c6beb3
dai <1lanyeue@gmail.com>
2023-05-11 21:05:17 +0700

    fix github

--- app/components/Modals/LoginModal.tsx
+++ app/components/Modals/LoginModal.tsx
@@ -102,12 +102,12 @@ const LoginModal = () => {
         icon={FcGoogle}
         onClick={() => signIn('google')}
       />
-      {/*<Button */}
-      {/*  outline */}
-      {/*  label="Continue with Github"*/}
-      {/*  icon={AiFillGithub}*/}
-      {/*  onClick={() => signIn('github')}*/}
-      {/*/>*/}
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => signIn('github')}
+      />
       <div className="
       text-neutral-500 text-center mt-4 font-light">
         <p>First time using app?
--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -104,12 +104,12 @@ const RegisterModal= () => {
         icon={FcGoogle}
         onClick={() => signIn('google')} 
       />
-      {/*<Button */}
-      {/*  outline */}
-      {/*  label="Continue with Github"*/}
-      {/*  icon={AiFillGithub}*/}
-      {/*  onClick={() => signIn('github')}*/}
-      {/*/>*/}
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => signIn('github')}
+      />
       <div 
         className="
           text-neutral-500 
